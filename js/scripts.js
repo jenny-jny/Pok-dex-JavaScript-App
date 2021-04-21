535e55066bc41687a67f048c2de708c66203062b
jenny-jny <jenny.yu.0414@gmail.com>
2021-04-20 21:32:02 -0500

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let myName = 'Bob';
+document.write(myName);
+
+myName = 'John Doe';
+document.write(myName);
+
+alert(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
