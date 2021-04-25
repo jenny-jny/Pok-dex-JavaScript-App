041181c8a6f23041938e6e730e795f318ba5252a
jenny-jny <jenny.yu.0414@gmail.com>
2021-04-24 21:09:35 -0500

    Create a for loop that iterates over each object in the pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
 let pokemonList = [{name : Bulbasaur, height : 7, type : ['grass', 'poison']}, {name : Charmander, height : 6, type : 'fire'}, {name : squirtle, height : 5, type : water}];
+
+for(let i = 0; i < pokemonList.length; i++){
+  document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')');
+}
