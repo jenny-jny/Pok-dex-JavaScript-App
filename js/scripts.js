83e4f5af6e34d8d4c8cf17f505051478222bee94
jenny-jny <jenny.yu.0414@gmail.com>
2021-04-24 22:27:20 -0500

    Iterate over array of pokemon objects and print according to height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-let pokemonList = [{name : 'bulbasaur', height : 7, type : ['grass', 'poison']}, {name : 'charmander', height : 6, type : 'fire'}, {name : 'squirtle', height : 5, type : 'water'}];
+//array of pokemon objects
+let pokemonList = [{name : 'bulbasaur', height : 7, type : ['grass', 'poison']}, {name : 'charmander', height : 6, type : ['fire']}, {name : 'squirtle', height : 5, type : ['water']}];
 
+//iterate over array of pokemon objects
 for(let i = 0; i < pokemonList.length; i++){
-  console.log(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')');
+  //big pokemons have height above 6
+  if(pokemonList[i].height > 6){
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!' + '<br/>');
+  //average pokemons have height equal to 6
+  }else if(pokemonList[i].height === 6){
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s average!' + '<br/>');
+  //small pokemons have height below 6
+  }else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s small!' + '<br/>');
+  }
 }
