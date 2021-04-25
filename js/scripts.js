a48836879af31feaf5da191d64c5c7e14688b924
jenny-jny <jenny.yu.0414@gmail.com>
2021-04-24 21:14:33 -0500

    Update pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-let pokemonList = [{name : Bulbasaur, height : 7, type : ['grass', 'poison']}, {name : Charmander, height : 6, type : 'fire'}, {name : squirtle, height : 5, type : water}];
+let pokemonList = [{name : 'bulbasaur', height : 7, type : ['grass', 'poison']}, {name : 'charmander', height : 6, type : 'fire'}, {name : 'squirtle', height : 5, type : 'water'}];
 
 for(let i = 0; i < pokemonList.length; i++){
-  document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')');
+  console.log(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')');
 }
