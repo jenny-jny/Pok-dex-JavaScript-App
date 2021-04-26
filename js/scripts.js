d83fcae69436b917ae7d05323e6eb51467962df6
jenny-jny <jenny.yu.0414@gmail.com>
2021-04-25 20:03:45 -0500

    Update paragraph format

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ let pokemonList = [{name : 'bulbasaur', height : 7, type : ['grass', 'poison']},
 for(let i = 0; i < pokemonList.length; i++){
   //big pokemons have height above 6
   if(pokemonList[i].height > 6){
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!' + '<br/>');
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!' + '</p>');
   //average pokemons have height equal to 6
   }else if(pokemonList[i].height === 6){
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s average!' + '<br/>');
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s average!' + '</p>');
   //small pokemons have height below 6
   }else {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s small!' + '<br/>');
+    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s small!' + '</p>');
   }
 }
