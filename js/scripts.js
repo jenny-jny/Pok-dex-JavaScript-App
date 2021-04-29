cbc0f5ffbd50332099ffc63493926fc274a1ffdf
jenny-jny <jenny.yu.0414@gmail.com>
2021-04-29 12:31:46 -0500

    Replaced for loop with forEach loop to iterate over pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
 //array of pokemon objects
-let pokemonList = [{name : 'bulbasaur', height : 7, type : ['grass', 'poison']}, {name : 'charmander', height : 6, type : ['fire']}, {name : 'squirtle', height : 5, type : ['water']}];
-
-//iterate over array of pokemon objects
-for(let i = 0; i < pokemonList.length; i++){
-  //big pokemons have height above 6
-  if(pokemonList[i].height > 6){
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!' + '</p>');
-  //average pokemons have height equal to 6
-  }else if(pokemonList[i].height === 6){
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s average!' + '</p>');
-  //small pokemons have height below 6
-  }else {
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s small!' + '</p>');
+let pokemonList = [
+  {
+    name : 'bulbasaur', 
+    height : 7, 
+    type : ['grass', 'poison']
+  }, 
+  {
+    name : 'charmander', 
+    height : 6, 
+    type : ['fire']
+  }, 
+  {
+    name : 'squirtle', 
+    height : 5, 
+    type : ['water']
   }
-}
+];
+
+//using for loop to iterate over array of pokemon objects
+// for(let i = 0; i < pokemonList.length; i++){
+//   //big pokemons have height above 6
+//   if(pokemonList[i].height > 6){
+//     document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!' + '</p>');
+//   //average pokemons have height equal to 6
+//   }else if(pokemonList[i].height === 6){
+//     document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s average!' + '</p>');
+//   //small pokemons have height below 6
+//   }else {
+//     document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s small!' + '</p>');
+//   }
+// }
+
+//using foreach loop to iterate over array of pokemon objects
+pokemonList.forEach(function(pokemon){
+  document.write('<p>' + 'Name: ' + pokemon.name + ', ' + 'height: ' + pokemon.height + ', ' + 'type: ' + pokemon.type + '</p>');
+})
