7bada34a4077fd7c71e454305439e2b04678df4c
jenny-jny <jenny.yu.0414@gmail.com>
2021-05-04 18:57:20 -0500

    Revert "Add pokeball styling to pokemon list buttons"
    This reverts commit 8abb850700539d5780b0ca1270d8c6c1fc7ad139.

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,19 @@ let pokemonRepository = (function(){
       type : ['water']
     }
   ];
+  function add(pokemon){
+    //validate if the pokemon is an object
+    if(typeof pokemon === 'object' && pokemon !== null){
+      //validate if the parameters of pokemon has the correct keys
+      let pokemonKeys = Object.keys(pokemon);
+      if(pokemonKeys.length === 3 && pokemonKeys[0] === 'name' && pokemonKeys[1] === 'height' && pokemonKeys[2] === 'type'){     
+        pokemonList.push(pokemon);
+      }
+    }
+  }
+  function getAll(){
+    return pokemonList;
+  }
   function addListItem(pokemon){
     let unorderedList = document.querySelector('.pokemon-list');
   
@@ -29,12 +42,36 @@ let pokemonRepository = (function(){
     listItem.appendChild(button);
     unorderedList.appendChild(listItem);
   }
+
+  //filter by name from array of pokemon objects
+  // function filterByName(pokemon){
+  //   return pokemonList.filter(pokemon1 => pokemon1.name === pokemon.name); //pokemon1 is an pokemon object in the pokemon array
+  // }
+  
   return {
+    add: add,
+    getAll: getAll,
     addListItem: addListItem
+    // filterByName: filterByName
   };
-})();
+})()
 
 //foreach loop to iterate over array of pokemon objects
 pokemonRepository.getAll().forEach(function(pokemon){
   pokemonRepository.addListItem(pokemon);
 })
+
+//add pokemon to array of pokemon objects
+// pokemonRepository.add({name: 'caterpie', height: 3, type: ['bug']});
+
+//foreach loop to iterate over array of pokemon objects
+// pokemonRepository.getAll().forEach(function(pokemon){
+//   document.write('<p>' + 'Name: ' + pokemon.name + ', ' + 'height: ' + pokemon.height + ', ' + 'type: ' + pokemon.type + '</p>');
+// })
+
+//filter by name from array of pokemon objects
+// let pokemonArray = pokemonRepository.filterByName({name: 'squirtle'});
+
+// pokemonArray.forEach(function(pokemon){
+//   document.write('<p>' + 'Name: ' + pokemon.name + ', ' + 'height: ' + pokemon.height + ', ' + 'type: ' + pokemon.type + '</p>');
+// })
