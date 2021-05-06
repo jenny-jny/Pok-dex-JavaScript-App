7550f2f590fe7ddacf4696a9af15af8d466b00bb
jenny-jny <jenny.yu.0414@gmail.com>
2021-05-06 10:24:58 -0500

    Add loadList function, call it, add comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ let pokemonRepository = (function(){
   let pokemonList = [];
   let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+  //add pokemon to pokemon list after passing validations
   function add(pokemon){
     //validate if the pokemon is an object
     if(typeof pokemon === 'object' && pokemon !== null){
@@ -14,10 +15,12 @@ let pokemonRepository = (function(){
     }
   }
 
+  //get all pokemons from pokemon list
   function getAll(){
     return pokemonList;
   }
 
+  //add buttons to list items then add list items to unordered pokemon list; call event listener
   function addListItem(pokemon){
     let unorderedList = document.querySelector('.pokemon-list');
   
@@ -33,13 +36,32 @@ let pokemonRepository = (function(){
     addEventListenerOnClick(button, pokemon);
   }
 
+  //print pokemon name to console
   function showDetails(pokemon){
     console.log(pokemon.name);
   }
 
+  //add event listener (to print pokemon  name to console) on clicking the button and print pokemon name to console
   function addEventListenerOnClick(button, pokemon){
     button.addEventListener('click', function(){
       showDetails(pokemon);
+    })
+  }
+
+  //load pokemon name and details URL from pokeapi and call add function (to add pokemon to pokemon list after passing validations)
+  function loadList(){
+    return fetch(apiURL).then(function (response){
+      return response.json();
+    }).then(function(json){
+      json.results.forEach(function(item){
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function(e){
+      console.error(e);
     })
   }
 
@@ -51,10 +73,14 @@ let pokemonRepository = (function(){
   };
 })()
 
-//foreach loop to iterate over array of pokemon objects
-pokemonRepository.getAll().forEach(function(pokemon){
-  pokemonRepository.addListItem(pokemon);
-})
-
 //add pokemon to array of pokemon objects
-pokemonRepository.add({name: 'caterpie', height: 3, type: ['bug']});
+// pokemonRepository.add({name: 'caterpie', height: 3, type: ['bug']});
+
+//load pokemons 
+pokemonRepository.loadList().then(function(){
+  //Now the data is loaded!
+  //foreach loop to iterate over array of pokemon objects and add pokemon to pokemon list item
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  })
+})
