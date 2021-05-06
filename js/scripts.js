d2800ae3d7f4ad8a40a01cb2ca51d337a42bf92f
jenny-jny <jenny.yu.0414@gmail.com>
2021-05-06 09:55:11 -0500

    Add add pokemon function and call it

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,22 @@
 let pokemonRepository = (function(){
   let pokemonList = [];
   let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  function add(pokemon){
+    //validate if the pokemon is an object
+    if(typeof pokemon === 'object' && pokemon !== null){
+      //validate if the parameters of pokemon has the correct keys
+      let pokemonKeys = Object.keys(pokemon);
+      if(pokemonKeys.length === 3 && pokemonKeys[0] === 'name' && pokemonKeys[1] === 'height' && pokemonKeys[2] === 'type'){     
+        pokemonList.push(pokemon);
+      }
+    }
+  }
+
   function getAll(){
     return pokemonList;
   }
+
   function addListItem(pokemon){
     let unorderedList = document.querySelector('.pokemon-list');
   
@@ -19,17 +32,22 @@ let pokemonRepository = (function(){
 
     addEventListenerOnClick(button, pokemon);
   }
+
   function showDetails(pokemon){
     console.log(pokemon.name);
   }
+
   function addEventListenerOnClick(button, pokemon){
     button.addEventListener('click', function(){
       showDetails(pokemon);
     })
   }
+
   return {
+    add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList
   };
 })()
 
@@ -37,3 +55,6 @@ let pokemonRepository = (function(){
 pokemonRepository.getAll().forEach(function(pokemon){
   pokemonRepository.addListItem(pokemon);
 })
+
+//add pokemon to array of pokemon objects
+pokemonRepository.add({name: 'caterpie', height: 3, type: ['bug']});
